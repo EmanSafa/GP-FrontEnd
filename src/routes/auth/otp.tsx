00a6faf6a9928b0ae165002405bf56b962ed1257
EmanSafa <emansafa55@gmail.com>
2025-11-03 20:38:12 +0200

    fix: Update OTP route to use the correct OTP component

--- src/routes/auth/otp.tsx
+++ src/routes/auth/otp.tsx
@@ -1,6 +1,6 @@
-import { OTPForm } from "@/components/ui/Auth-Forms/otp-form";
+import OTP from "@/components/Pages/Auth/OTPPage";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/auth/otp")({
-  component: OTPForm,
+  component: OTP,
 });
