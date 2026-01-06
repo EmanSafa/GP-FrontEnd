e96fb6629d367521cbd6576a70bb7376e7d766da
EmanSafa <emansafa55@gmail.com>
2026-01-06 19:17:40 +0200

    feat: Add dashboard and account routes with authentication and role-based access control.

--- src/routes/_main/dashboard/index.tsx
+++ src/routes/_main/dashboard/index.tsx
@@ -1,18 +1,8 @@
 import ProductsDashboard from '@/components/Pages/Dashboard/Products/ProductsDashboard';
-import { useAuthStore } from '@/store/authStore'
-import { createFileRoute, redirect } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_main/dashboard/')({
-  beforeLoad: () => {
-    const { isAuthenticated } = useAuthStore.getState();
-    
-    // Require authentication
-    if (!isAuthenticated) {
-      throw redirect({
-        to: '/auth/login',
-      });
-    }
-  },
+
   component: ProductsDashboard,
 })
 
