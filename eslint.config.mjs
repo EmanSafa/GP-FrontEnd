61d17db7d764a8641ee08c2c821bd916d667c8a3
EmanSafa <emansafa55@gmail.com>
2026-06-21 01:01:08 +0300

    chore: add husky, lint-staged, and prettier configuration to project

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config({
+  files: ['**/*.ts', '**/*.tsx'],
+  languageOptions: {
+    parserOptions: {
+      project: ['./tsconfig.eslint.json'], //  Type-aware linting
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  extends: [
+    eslint.configs.recommended,             // Base JS rules
+    ...tseslint.configs.recommended,        // Basic TS rules
+    ...tseslint.configs.recommendedTypeChecked, // Type-aware TS rules
+  ],
+  rules: {
+    '@typescript-eslint/array-type': 'error',
+    '@typescript-eslint/no-duplicate-type-constituents': 'error'
+    // Add more rules as needed
+  },
+});
